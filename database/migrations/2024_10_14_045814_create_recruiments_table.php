cef912b080d987c1cae92cc3293eb0365a96b603
Nguyễn Văn Hoàng Tâm <nguyenvanhoangtam2556@gmail.com>
2024-10-14 12:18:12 +0700

    add migrations

---
+++ database/migrations/2024_10_14_045814_create_recruiments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recruiments', function (Blueprint $table) {
+            $table->id();
+            $table->string('location');
+            $table->string('workplace');
+            $table->text('address');
+            $table->text('job_description');
+            $table->date('date');
+            $table->string('working_time');
+            $table->string('interest');
+            $table->string('requirement');
+            $table->string('age');
+            $table->string('level');
+            $table->string('file');
+            $table->foreignId('field_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('type_work_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('working_place_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recruiments');
+    }
+};
---
+++ database/migrations/2024_10_14_045825_create_apply_works_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apply_works', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->binary('sex');
+            $table->date('dob');
+            $table->text('place_of_birth');
+            $table->string('current_residence');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('facebook');
+            $table->string('level');
+            $table->string('file_cv');
+            $table->string('long_day_work');
+            $table->string('overtime');
+            $table->string('previous_workplace');
+            $table->string('experience');
+            $table->foreignId('recruitment_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apply_works');
+    }
+};
